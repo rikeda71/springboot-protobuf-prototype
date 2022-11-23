7fa5565b5b3bb58385e34753fc5065139bd217e0
Ryuya Ikeda <rikeda71@gmail.com>
2022-11-23 12:36:03 +0900

    :tada: setup project

---
+++ src/main/java/com/s14t284/demo/smokeproto/SmokeprotoApplication.java
@@ -0,0 +1,13 @@
+package com.s14t284.demo.smokeproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokeprotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokeprotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s14t284/demo/smokeproto/SmokeprotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s14t284.demo.smokeproto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeprotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
